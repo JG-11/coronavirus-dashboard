8ef0e728fc4015d7a28295803d5ce03145404398
Genaro Almaraz <jg08950@gmail.com>
2020-03-25 21:44:40 -0600

    Best Chart Component structure

--- src/Chart.js
+++ src/Chart.js
@@ -1,24 +1,25 @@
 import React from 'react'
-import { HorizontalBar } from 'react-chartjs-2'
 
+import { HorizontalBar, Polar } from 'react-chartjs-2'
 
-const Chart = props => {
+const HorizontalBarChart = props => {
     const mexico = props.mexico.replace(',', '')
+    const usa = props.usa.replace(',', '')
     const china = props.china.replace(',', '')
     const italy = props.italy.replace(',', '')
     const spain = props.spain.replace(',', '')
 
     const data = {
-        labels: ['México', 'China', 'Italia', 'España'],
+        labels: ['México', 'USA', 'China', 'Italia', 'España'],
         datasets: [
             {
-            label: 'Casos confirmados',
-            backgroundColor: 'rgba(0, 167, 241, 0.2)',
-            borderColor: 'rgba(0, 167, 241, 1)',
-            borderWidth: 1,
-            hoverBackgroundColor: 'rgba(0, 167, 241, 0.4)',
-            hoverBorderColor: 'rgba(0, 167, 241, 1)',
-            data: [mexico, china, italy, spain]
+                label: 'Casos confirmados',
+                backgroundColor: 'rgba(0, 167, 241, 0.2)',
+                borderColor: 'rgba(0, 167, 241, 1)',
+                borderWidth: 1,
+                hoverBackgroundColor: 'rgba(0, 167, 241, 0.4)',
+                hoverBorderColor: 'rgba(0, 167, 241, 1)',
+                data: [mexico, usa, china, italy, spain]
             }
         ]
     }
@@ -28,4 +29,31 @@ const Chart = props => {
     )
 }
 
-export default Chart
+export const PolarChart = props => {
+    const mexico = props.mexico.replace(',', '')
+    const usa = props.usa.replace(',', '')
+    const china = props.china.replace(',', '')
+    const italy = props.italy.replace(',', '')
+    const spain = props.spain.replace(',', '')
+
+    const data = {
+        labels: ['México', 'USA', 'China', 'Italia', 'España'],
+        datasets: [
+            {
+                label: 'Total de recuperados',
+                backgroundColor: 'rgba(0, 167, 241, 0.2)',
+                borderColor: 'rgba(0, 167, 241, 1)',
+                borderWidth: 1,
+                hoverBackgroundColor: 'rgba(0, 167, 241, 0.4)',
+                hoverBorderColor: 'rgba(0, 167, 241, 1)',
+                data: [mexico, usa, china, italy, spain]
+            }
+        ]
+    }
+
+    return (
+        <Polar data={data}/> 
+    )
+}
+
+export default HorizontalBarChart
