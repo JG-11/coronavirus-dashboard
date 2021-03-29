49a6aea415a6db9fb80bea0f39e7aa22b54747bd
JG-11 <jg08950@gmail.com>
2021-03-28 20:00:39 -0600

    Remove API getCountryData()

--- src/API.js
+++ src/API.js
@@ -12,31 +12,4 @@ const getCountriesData = async() => {
   return countries_stat
 }
 
-export const getCountryData = async(name) => {
-  const response = await fetch("https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_country.php", {
-    "method": "GET",
-    "headers": {
-      "x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
-      "x-rapidapi-key": "4235b924b7mshc2c6867f165137dp103a73jsn7a30a56a550d"
-    }
-  })
-
-  const {countries_stat} = await response.json()
-
-  const data = []
-
-  countries_stat.forEach(country => {
-    if(country['country_name'] === name) {
-      const confirmed = country['cases']
-      const recovered = country['total_recovered']
-      const deaths = country['deaths']
-      const critical = country['serious_critical']
-
-      data.push({confirmed, deaths, recovered, critical})
-    }
-  })
-
-  return data
-}
-
 export default getCountriesData
